9fbe9b20bc12a99760369a741f85cc7fb28e225f
vinuth <vinuthsriarampath@outlook.com>
2024-10-27 21:13:36 +0530

    Added the post request for Institute registration with some test cases;

--- src/test/java/edu/icet/crm/controller/InstituteControllerTest.java
+++ src/test/java/edu/icet/crm/controller/InstituteControllerTest.java
@@ -2,7 +2,6 @@ package edu.icet.crm.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.icet.crm.model.Institute;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
