ff0abb0bb914631a2670d71c13dd1d93e4ce33b4
vinuth <vinuthsriarampath@outlook.com>
2024-11-02 20:53:37 +0530

    Student id Auto Generated

--- src/main/java/edu/icet/crm/service/StudentService.java
+++ src/main/java/edu/icet/crm/service/StudentService.java
@@ -1,9 +1,15 @@
 package edu.icet.crm.service;
 
 import edu.icet.crm.model.Student;
-import jakarta.validation.Valid;
+
+import java.util.List;
 
 public interface StudentService {
     void registerStudent(Student student);
+
     Student searchStudentById(String studentId);
+
+    List<Student> getAllStudents();
+
+    String generateStudentId();
 }
--- src/main/java/edu/icet/crm/service/impl/StudentServiceImpl.java
+++ src/main/java/edu/icet/crm/service/impl/StudentServiceImpl.java
@@ -7,18 +7,41 @@ import edu.icet.crm.repository.StudentRepository;
 import edu.icet.crm.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
-    private final ObjectMapper objectMapper;
+    private final ObjectMapper mapper;
+    @Override
+    public String generateStudentId() {
+        List<Student> allStudents = getAllStudents();
+        allStudents.sort((stu1,stu2)->{
+            int id1 = Integer.parseInt(stu1.getId().split("S")[1]);
+            int id2 = Integer.parseInt(stu2.getId().split("S")[1]);
+            return Integer.compare(id1,id2);
+        });
+        int idNum=Integer.parseInt(allStudents.get(allStudents.size()-1).getId().split("S")[1])+1;
+        return "S"+idNum;
+    }
+    @Override
+    public List<Student> getAllStudents() {
+        List<Student> allStudents= new ArrayList<>();
+        studentRepository.findAll().forEach(studentEntity -> allStudents.add(mapper.convertValue(studentEntity, Student.class)));
+        return allStudents;
+    }
+
     @Override
     public void registerStudent(Student student) {
-        studentRepository.save(objectMapper.convertValue(student, StudentEntity.class));
+        student.setId(generateStudentId());
+        studentRepository.save(mapper.convertValue(student, StudentEntity.class));
     }
     @Override
     public Student searchStudentById(String studentId) {
-        return objectMapper.convertValue(studentRepository.findByid(studentId), Student.class);
+        return mapper.convertValue(studentRepository.findByid(studentId), Student.class);
     }
 }
