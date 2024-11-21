fcfcdc10384233823c61ab7ff0c5df9b3ae63cfd
vinuth <vinuthsriarampath@outlook.com>
2024-11-21 17:25:56 +0530

    Organize the code structure in the EmailService

--- src/main/java/edu/icet/crm/service/EmailService.java
+++ src/main/java/edu/icet/crm/service/EmailService.java
@@ -3,7 +3,11 @@ package edu.icet.crm.service;
 
 public interface EmailService {
     void sendInstituteOtpEmail(String to, String subject, String body);
+
     void sendInstituteRegistrationSuccessful(String to,String subject, String userName,String instituteName);
+
     void sentTeacherRegistrationSuccessful(String to,String subject,String userName);
+
     void sentStudentRegistrationSuccessful(String to,String subject,String userName);
+
 }
